20ce3397d2543a6e3d39b9cd132b0d369144955e
js345 <1994yxf@gmail.com>
2016-03-21 13:58:28 -0500

    Create osx.sh

---
+++ osx.sh
@@ -0,0 +1,5 @@
+brew install libxml2 libxslt
+brew install libtiff libjpeg webp little-cms2
+xcode-select --install
+sudo pip install newspaper
+curl https://raw.githubusercontent.com/codelucas/newspaper/master/download_corpora.py | python2.7
