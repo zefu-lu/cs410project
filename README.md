3dad978cd928076b943cef87e9dbda8d5d5629ed
Yiyi Wang <shd101wyy@sina.com>
2016-04-14 21:22:05 -0500

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cs410project
+## Front-End
+#### Setup
+```sh
+npm install -g webpack
+npm install
+```
+#### Build
+```sh
+./front.sh
+```
--- www/bundle.js
+++ www/bundle.js
@@ -56,6 +56,24 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var App = function (_React$Component) {
+	  _inherits(App, _React$Component);
+
+	  function App(props) {
+	    _classCallCheck(this, App);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(App).call(this, props));
+	  }
+
+	  return App;
+	}(_react2.default.Component);
+
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
