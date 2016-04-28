50057172445171d82d92d5ea47c3bec83cae6bf7
BtXin <weixin1@wirelessprvnat-172-17-195-12.near.illinois.edu>
2016-04-27 22:44:54 -0500

    render

--- searchengine/server.py
+++ searchengine/server.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, abort, jsonify
 from flask.ext.restful import Resource, Api
 from searchAPI import SearchAPI
+from flask import render_template
 
 app = Flask(__name__)
 app.config.from_object('config')
 api = Api(app)
+
+@app.route('/')
+@app.route('/<name>')
+def index(name=None):
+    return render_template('../www/index.html', name=name)
+
 
 api.add_resource(SearchAPI, '/')
 
